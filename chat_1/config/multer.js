c70f92a74ca2beb31ba71cbfb33b7e22cce2ef45
Hyun-juhee <juhe1998@naver.com>
2020-09-29 17:37:32 +0900

    생략됐었던 config/multer.js 파일 업로드

---
+++ chat_1/config/multer.js
@@ -0,0 +1,21 @@
+const multer = require('multer');
+const multerS3 = require('multer-s3');
+const aws = require('aws-sdk');
+aws.config.loadFromPath(__dirname + '/awsconfig.json');
+const s3 = new aws.S3();
+
+const upload = multer({
+    storage: multerS3({
+        s3: s3,
+        bucket: '버킷이름',
+        acl: 'public-read',
+        key: function(req, file, cb) {
+            cb(null, Math.floor(Math.random() * 1000).toString() + Date.now() + '.' + file.originalname.split('.').pop());
+        }
+    }),
+    limits: {
+        fileSize: 1000 * 1000 * 10
+    }
+});
+
+module.exports = upload;
